ce2a6c418152e45b4d8e45e4a67189beac608146
ABlued <skwodhkswkd@naver.com>
2020-10-03 01:37:12 +0900

    온도를 화면에 띄우기

--- App.js
+++ App.js
@@ -4,16 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import Loading from "./loading";
 import axios from "axios";
 import * as Location from 'expo-location';
+import Weather from './Weather';
 
 const API_KEY = "a72b4fa53125a6cfcb6c0909aa496d09";
 
 
 export default class extends React.Component{
   state = {
-    isLoading : true
+    isLoading : true,
+
   };
   getWeather = async(latitude, longitude) => {
     const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    this.setState({isLoading: false, temp: data.main.temp})
     console.log(data);
   };
   
@@ -24,7 +27,7 @@ export default class extends React.Component{
         coords : {latitude, longitude}
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({isLoading:true})
+      this.setState({isLoading:false})
     } catch (error) {
       Alert.alert("Can't find you","So sad");
     }
@@ -34,8 +37,8 @@ export default class extends React.Component{
     this.getLocation();
   }
   render(){
-    const { isLoading } = this.state;
-    return isLoading ? <Loading/> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading/> : <Weather temp={Math.round(temp)}/>;
   }
 }
 
