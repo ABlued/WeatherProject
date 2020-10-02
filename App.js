7686297e720cccdc4c6b64671ac2663df70b4dbf
ABlued <skwodhkswkd@naver.com>
2020-10-03 01:08:15 +0900

    위도와 경도로 날씨받아내서 출력하기

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Alert} from "react-native";
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./loading";
+import axios from "axios";
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "a72b4fa53125a6cfcb6c0909aa496d09";
+
+
+export default class extends React.Component{
+  state = {
+    isLoading : true
+  };
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    console.log(data);
+  };
+  
+  getLocation = async () =>{
+    try {
+      await Location.requestPermissionsAsync(); 
+      const {
+        coords : {latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading:true})
+    } catch (error) {
+      Alert.alert("Can't find you","So sad");
+    }
+   
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const { isLoading } = this.state;
+    return isLoading ? <Loading/> : null;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
 });
